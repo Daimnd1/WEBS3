8f418d4fe6911e8c286b1a2dbda1f36cf924cc5d
vld31 <vlad.tomulescu317@gmail.com>
2025-10-04 22:19:55 +0200

    Shopping cart UI

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,20 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 
-Route::get('/about', function () {
-    return Inertia::render('About');
-});
+Route::get('/', function () {
+    return Inertia::render('Home');
+})->name('home');
+
+Route::get('/products', function () {
+    $category = request('category');
+    return Inertia::render('Products', [
+        'category' => $category
+    ]);
+})->name('products');
+
+Route::get('/login', function () {
+    return Inertia::render('loginpage');
+})->name('loginpage');
 
 Route::get('/cart', function () {
     return Inertia::render('CartVisualizationDemo');
